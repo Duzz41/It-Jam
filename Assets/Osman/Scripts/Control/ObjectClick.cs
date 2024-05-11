dde3da630799db0de06c4e2c12d2148afff0e9d5
Duzz41 <osmansisman433@gmail.com>
2024-05-11 16:00:38 +0300

    New ClickableObject Added

---
+++ Assets/Osman/Scripts/Control/ObjectClick.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClick : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        // Fare imlecinin konumunu al
+        Vector3 mousePosition = Input.mousePosition;
+
+        // Fare imleci dünya koordinatlarına dönüştür
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        // Raycast işlemi
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Fare imlecinin dünya koordinatları ile nesnenin dünya koordinatlarını karşılaştır
+            if (hit.collider.gameObject == gameObject)
+            {
+                Debug.Log("Nesneye tıklandı!");
+                // Nesne ile imlecin konumu çakışıyor, istediğiniz işlemi yapabilirsiniz
+            }
+        }
+    }
+}
